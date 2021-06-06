1e92e00a17bac47c12830a70c795808abe0787a2
Semviel <semviel22@gmail.com>
2021-06-06 16:31:05 +0300

    force portrait mode for the phones

--- app/src/main/java/com/korefu/developerslife/MainActivity.kt
+++ app/src/main/java/com/korefu/developerslife/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.korefu.developerslife
 
+import android.annotation.SuppressLint
+import android.content.pm.ActivityInfo
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("SourceLockedOrientationActivity")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        if (resources.getBoolean(R.bool.portrait_only)) {
+            requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT;
+        }
         setContentView(R.layout.activity_main)
     }
 }
